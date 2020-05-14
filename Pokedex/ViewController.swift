239a53426eda59e59480587577d5b86442a43ca1
David Murillo <d.murillo28@yahoo.com>
2020-05-13 20:46:22 -0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by David Murillo on 5/13/20.
+//  Copyright © 2020 MuriTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
